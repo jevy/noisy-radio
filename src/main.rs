e267aa04dd10c2af0425b60b0567307805924788
Jevin Maltais <jevin@quickjack.ca>
2023-02-22 09:05:47 -0500

    Using Rodio instead. It works

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use kira::{
-    manager::{
-        AudioManager, AudioManagerSettings,
-        backend::cpal::CpalBackend,
-    },
-    sound::{static_sound::{StaticSoundData, StaticSoundSettings}},
-};
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source};
 
 fn main() {
-    let mut manager: AudioManager = AudioManager::<CpalBackend>::new(AudioManagerSettings::default()).unwrap();
-    let sound_data = StaticSoundData::from_file("./src/nature.mp3", StaticSoundSettings::new());
-    match sound_data {
-        Ok(it) => manager.play(it),
-        Err(_) => todo!(),
-    };
-}
+    // Get a output stream handle to the default physical sound device
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    // Load a sound from a file, using a path relative to Cargo.toml
+    let file = BufReader::new(File::open("src/nature.mp3").unwrap());
+    // Decode that sound file into a source
+    let source = Decoder::new(file).unwrap();
+    // Play the sound directly on the device
+    stream_handle.play_raw(source.convert_samples());
 
+    // The sound plays in a separate audio thread,
+    // so we need to keep the main thread alive while it's playing.
+    std::thread::sleep(std::time::Duration::from_secs(5));
+}
