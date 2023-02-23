67d549f7b40a77fd29005c1eceffe17ac67b199f
Jevin Maltais <jevin@quickjack.ca>
2023-02-23 18:24:27 -0500

    A bit more modular

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use rodio::dynamic_mixer::{self, DynamicMixer};
 
 use std::{io, thread, time::Duration};
 use tui::{
+    backend::Backend,
     backend::CrosstermBackend,
     widgets::{Widget, Block, Borders},
     layout::{Layout, Constraint, Direction},
@@ -17,8 +18,53 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
+struct Radio {
+    tuned_channel: i8,
+}
+
+struct Channel {
+    freq: i8,
+}
+
 fn main() -> Result<(), io::Error> {
     
+    enable_raw_mode()?;
+    execute!(
+        std::io::stdout(),
+        EnterAlternateScreen,
+        EnableMouseCapture
+    )?;
+    let backend = CrosstermBackend::new(std::io::stdout());
+    let mut terminal = Terminal::new(backend)?;
+
+    let result = run_app(&mut terminal);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(e) = result {
+        println!("{}", e.to_string());
+    }
+
+    Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> Result<(), std::io::Error> {
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Block")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
     let sink = Sink::try_new(&stream_handle).unwrap();
@@ -35,37 +81,10 @@ fn main() -> Result<(), io::Error> {
     tx.add(static_source);
     tx.add(nature_source);
     sink.append(rx);
-
-    // setup terminal
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-
-    terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default()
-            .title("Block")
-            .borders(Borders::ALL);
-        f.render_widget(block, size);
-    })?;
-
   
     sink.sleep_until_end();
 
-    // thread::sleep(Duration::from_millis(5000));
-
-    // restore terminal
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
-
-    Ok(())
+    return Ok(());
 }
 
 // fn main() {
