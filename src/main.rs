e95ddf050dc5f2b9f63123242dd916f5d76c5cb2
Jevin Maltais <jevin@quickjack.ca>
2023-02-24 08:32:47 -0500

    Able to control the audio mix between channels

--- src/main.rs
+++ src/main.rs
@@ -79,24 +79,21 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> Result<(), std::io::Error>
 
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 
-    let sink = Sink::try_new(&stream_handle).unwrap();
-
-    // Load a sound from a file, using a path relative to Cargo.toml
+    let nature_sink = Sink::try_new(&stream_handle).unwrap();
     let nature_file = BufReader::new(File::open("src/nature.mp3").unwrap());
-    let static_file = BufReader::new(File::open("src/radio-static.mp3").unwrap());
-    // Decode that sound file into a source
     let nature_source = Decoder::new(nature_file).unwrap();
-    let static_source = Decoder::new(static_file).unwrap();
-    
-    let (tx, mut rx) = dynamic_mixer::mixer(1, 48000);
+    nature_sink.append(nature_source);
 
-    tx.add(static_source);
-    tx.add(nature_source);
-    sink.append(rx);
+    let static_sink = Sink::try_new(&stream_handle).unwrap();
+    let static_file = BufReader::new(File::open("src/radio-static.mp3").unwrap());
+    let static_source = Decoder::new(static_file).unwrap();
+    static_sink.append(static_source);
 
     let mut radio = Radio::new();
   
     loop {
+
+        static_sink.set_volume((radio.tuned_channel as f32) / 100.0);
 
         terminal.draw(|f| {
             let size = f.size();
@@ -104,7 +101,7 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> Result<(), std::io::Error>
                 .style(Style::default().fg(Color::Yellow))
                 .alignment(Alignment::Center);
             f.render_widget(freq_text, size);
-        })?;
+        });
 
         if let Key(key) = event::read()? {
             match key.code {
